c226c83e1c19015b7b85d4e02c841e36826b9a64
melion11 <gphooenix@gmail.com>
2023-09-11 21:58:11 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
-import "./index.css";
+import './styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
